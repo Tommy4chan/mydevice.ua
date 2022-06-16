0115c261c738afd93c335dcb009a6a0a4bbd4031
Tommy4chan <tommy4chan@gmail.com>
2022-06-16 17:02:11 +0300

    first commit

---
+++ storage/framework/views/6de18ec6f7e550d6a3babbd2a4438dd303a5f078.php
@@ -0,0 +1,38 @@
+<?php $__env->startSection('title'); ?>Головна сторінка - MyDevice.ua <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('main_content'); ?>
+
+<header>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="img/1.webp" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/2.webp" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/3.webp" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+</header>
+
+<h1>Головна сторінка</h1>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.defaultLayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Projects\OpenServer\domains\mydevice.ua\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a8aa04e1ad515a50a437ce4f55c6dcaa3512a207.php
@@ -0,0 +1,52 @@
+<?php $__env->startSection('title'); ?>Головна сторінка - MyDevice.ua <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('main_content'); ?>
+
+<header>
+    <div class="container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="img/1.webp" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/2.webp" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/3.webp" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+</header>
+
+<section class="popular-products">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 d-flex justify-content-center">
+                <?php echo $__env->make('layouts.productCard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center">
+                <?php echo $__env->make('layouts.productCard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.defaultLayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Projects\OpenServer\domains\mydevice.ua\resources\views/home.blade.php ENDPATH**/ ?>
